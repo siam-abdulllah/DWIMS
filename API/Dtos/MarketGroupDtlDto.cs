a67d4cb65679ebfda8fe669354d74099822752eb
Siam@bdullah <abdulllahsiam@gmail.com>
2021-07-13 17:21:37 +0600

    Siam_1372021_2

---
+++ API/Dtos/MarketGroupDtlDto.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace API.Dtos
+{
+    public class MarketGroupDtlDto
+    {
+        public int Id { get; set; }
+        public int MstId { get; set; }
+        public string MarketCode { get; set; }
+        public string MarketName { get; set; }
+        public string Status { get; set; }
+
+    }
+}
