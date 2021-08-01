ab2fb4aed339fee676ffa52a66ac3d6453cbe750
dead-dance <70639757+dead-dance@users.noreply.github.com>
2021-08-01 08:54:59 +0600

    .

---
+++ API/Controllers/InvestmentAprController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Dtos;
+using API.Errors;
+using API.Helpers;
+using AutoMapper;
+using Core.Entities;
+using Core.Interfaces;
+using Core.Specifications;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace API.Controllers
+{
+    public class InvestmentAprController :  BaseApiController
+    {
+       
+    }
+}
