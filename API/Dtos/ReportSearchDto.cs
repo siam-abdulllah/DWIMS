a1589daeb84c3a75d7cb4cd0f47f2e18685221fe
dead-dance <70639757+dead-dance@users.noreply.github.com>
2021-08-12 17:08:31 +0600

    .

---
+++ API/Dtos/ReportSearchDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Dtos
+{
+    public class ReportSearchDto
+    {
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+
+    }
+}
