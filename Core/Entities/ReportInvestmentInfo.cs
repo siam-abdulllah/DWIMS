378034186dba5134f5d86f333ce84534aa832b50
dead-dance <70639757+dead-dance@users.noreply.github.com>
2021-08-16 16:57:24 +0600

    .
    .

--- Core/Entities/ReportInvestmentInfo.cs
+++ Core/Entities/ReportInvestmentInfo.cs
@@ -4,14 +4,22 @@ namespace Core.Entities
     public class ReportInvestmentInfo : BaseEntity
     {
         public string MarketCode { get; set; }
+        public string MarketName { get; set; }
         public string TerritoryCode { get; set; }
+        public string TerritoryName { get; set; }
         public string RegionCode { get; set; }
+        public string RegionName { get; set; }
         public string ZoneCode { get; set; }
+        public string ZoneName { get; set; }
         public string DivisionCode { get; set; }
+        public string DivisionName { get; set; }
         public string NationalCode { get; set; }
         public string SocietyId { get; set; }
+        public InvestmentSociety InvestmentSociety { get; set; }
         public string DoctorId { get; set; }
+        public DoctorInfo DoctorInfo { get; set; }
         public string InstituteId { get; set; }
+        public InstitutionInfo InstitutionInfo { get; set; }
         public string DonationType { get; set; }
         public string ExpenseDetail { get; set; }
         public string InvestmentAmount { get; set; }
