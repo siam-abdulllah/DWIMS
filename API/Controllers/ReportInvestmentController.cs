187df8ca4bb6bc5886dba86a3676660f692a170f
dead-dance <70639757+dead-dance@users.noreply.github.com>
2021-08-16 16:56:26 +0600

    .

--- API/Controllers/ReportInvestmentController.cs
+++ API/Controllers/ReportInvestmentController.cs
@@ -21,5 +21,25 @@ namespace API.Controllers
             _mapper = mapper;
             _investRepo = investRepo;
         }
+
+        [HttpGet("GetInstituteInvestment")]
+        //[Authorize(Roles = "Owner,Administrator")]
+        //[Authorize(Policy = "DetailUserPolicy")]
+        public async Task<ActionResult<IReadOnlyList<ReportInvestmentDto>>> GetInstituteInvestment([FromQuery] ReportInvestmentInfoSpecParams rptParrams)
+        {
+            string from = "", to = "";
+
+            var spec = new ReportInvestmentInfoSpecification(from, to);
+
+            var countSpec = new ReportInvestmentInfoSpecParamsWithFiltersForCountSpecificication(rptParrams);
+
+            var totalItems = await _investRepo.CountAsync(countSpec);
+
+            var posts = await _investRepo.ListAsync(spec);
+
+            var data = _mapper.Map<IReadOnlyList<ReportInvestmentInfo>, IReadOnlyList<ReportInvestmentDto>>(posts);
+
+            return Ok(new Pagination<ReportInvestmentDto>(rptParrams.PageIndex, rptParrams.PageSize, totalItems, data));
+        }
     }
 }
---
+++ Core/Specifications/ReportInvestmentInfoSpecParamsWithFiltersForCountSpecificication.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ReportInvestmentInfoSpecParamsWithFiltersForCountSpecificication : BaseSpecification<ReportInvestmentInfo>
+    {
+        public ReportInvestmentInfoSpecParamsWithFiltersForCountSpecificication(ReportInvestmentInfoSpecParams SpecParrams)
+           : base(x =>
+               (string.IsNullOrEmpty(SpecParrams.Search))
+           )
+        {
+        }
+        public ReportInvestmentInfoSpecParamsWithFiltersForCountSpecificication(int id)
+             : base(x => x.Id == id)
+        {
+            // AddInclude(x => x.Employee);
+        }
+    }
+}
