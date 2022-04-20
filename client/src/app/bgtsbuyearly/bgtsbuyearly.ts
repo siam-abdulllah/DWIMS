f878700f10f57ee9ae7e09214527f0db4a9b9f86
Rezwan1994 <rezwanulhaquerezwan@gmail.com>
2022-04-20 11:45:53 +0600

    Rezwan_20042022_1

---
+++ client/src/app/bgtsbuyearly/bgtsbuyearly.ts
@@ -0,0 +1,85 @@
+import { DatePipe } from "@angular/common";
+import { Component, ElementRef, OnInit, TemplateRef, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
+import { BsModalService,BsModalRef} from "ngx-bootstrap/modal";
+import { NgxSpinnerService } from "ngx-spinner";
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from "rxjs/internal/Observable";
+import { AccountService } from "../account/account.service";
+import { BudgetYearly, IBudgetYearly } from "../shared/models/budgetyearly";
+import { IEmployeeInfo } from "../shared/models/employeeInfo";
+import { BudgetYearlyService } from "../_services/budgetyearly.service";
+
+
+@Component({
+  selector: 'app-bgtyearly',
+  templateUrl: './bgtyearly.component.html',
+  styles: [
+  ],
+  providers: [DatePipe]
+})
+export class BgtSbuYearlyComponent implements OnInit {
+  @ViewChild('search', { static: false }) searchTerm: ElementRef;
+  @ViewChild('investmentRapidSearchModal', { static: false }) investmentInitSearchModal: TemplateRef<any>;
+  @ViewChild('submissionConfirmModal', { static: false }) submissionConfirmModal: TemplateRef<any>;
+  submissionConfirmRef: BsModalRef;
+  bsValue: Date = new Date();
+  today = new Date();
+  empId: string;
+  userRole:string;
+  empSbu:Observable<IEmployeeInfo>;
+  isAdmin: boolean = false;
+  dd = String(this.today.getDate()).padStart(2, '0');
+  mm = String(this.today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  yyyy = this.today.getFullYear();
+  todayDate = this.dd + this.mm + this.yyyy;
+  config = {
+    keyboard: false,
+    class: 'modal-lg',
+    ignoreBackdropClick: true
+  };
+  institutionType: string;
+  constructor(private accountService: AccountService,private router: Router,public bugetyearlyservice: BudgetYearlyService,private toastr: ToastrService, private modalService: BsModalService, private datePipe: DatePipe, 
+    private SpinnerService: NgxSpinnerService) { }
+  ngOnInit() {
+ 
+  }
+  confirmSubmission() {
+    debugger;
+    this.openSubmissionConfirmModal(this.submissionConfirmModal);
+  }
+  confirmSubmit() {
+    this.submissionConfirmRef.hide();
+    //this.submitInvestmentForm();
+  }
+  declineSubmit() {
+    this.submissionConfirmRef.hide();
+  }
+  openSubmissionConfirmModal(template: TemplateRef<any>) {
+    this.submissionConfirmRef = this.modalService.show(template, {
+      keyboard: false,
+      class: 'modal-md',
+      ignoreBackdropClick: true
+    });
+  }
+
+
+  resetPageLoad() {
+    this.bugetyearlyservice.budgetYearly = new BudgetYearly();
+  }
+
+  submitBgtYearly() {
+      this.SpinnerService.show();
+      this.bugetyearlyservice.submitBudgetYearly().subscribe(
+        res => {
+          this.bugetyearlyservice.budgetYearly = res as IBudgetYearly;
+          this.toastr.success('Submitted successfully', 'Investment');
+          this.resetPageLoad();
+        },
+        err => { 
+          console.log(err); 
+        }
+      );
+  }
+}
+
