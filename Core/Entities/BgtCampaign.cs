6ecf90676848878af692dfc6e91d462425650781
dead-dance <dead-dance@hotmail.com>
2022-06-02 13:08:19 +0600

    .

--- Core/Entities/BgtCampaign.cs
+++ Core/Entities/BgtCampaign.cs
@@ -9,6 +9,7 @@ namespace Core.Entities
         public int CompId { get; set; }
         public int DeptId { get; set; }
         public int Year { get; set; }
+        public string SBU { get; set; }
         public long Amount { get; set; }
         public string Remarks { get; set; }
         public int EnteredBy { get; set; }
