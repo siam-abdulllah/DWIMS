c54e35b99e3d7bf1438c06e2be130a26d1a670b4
Siam@bdullah <abdulllahsiam@gmail.com>
2022-06-21 09:13:38 +0600

    siam_migrate_21062022_1

---
+++ Infrastructure/Data/Migrations/20220621031143_InitialCreate_21062022_1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class InitialCreate_21062022_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApprovalAuthId",
+                table: "InvestmentRapidAppr");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ApproverId",
+                table: "InvestmentRapidAppr",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApproverId",
+                table: "InvestmentRapidAppr");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ApprovalAuthId",
+                table: "InvestmentRapidAppr",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
