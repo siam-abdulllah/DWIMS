7b2a57d640facf4bb12018202eb84ede862c7fc5
dead-dance <dead-dance@hotmail.com>
2022-07-04 16:52:36 +0600

    .

--- client/src/app/rptEmpWiseExp/rptEmpWiseExp.component.ts
+++ client/src/app/rptEmpWiseExp/rptEmpWiseExp.component.ts
@@ -18,6 +18,7 @@ import { CalendarCellViewModel } from 'ngx-bootstrap/datepicker/models';
 import { IApprovalAuthority } from '../shared/models/approvalAuthority';
 import { ISBU } from '../shared/models/sbu';
 import { BDCurrencyPipe } from '../bdNumberPipe';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'rptEmpWiseExp',
@@ -128,6 +129,20 @@ export class RptEmpWiseExpComponent implements OnInit {
       return;
     };
 }
+
+exportexcel(): void {
+  /* pass here the table id */
+  let element = document.getElementById('excel-table');
+  const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+  /* generate workbook and add the worksheet */
+  const wb: XLSX.WorkBook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+  XLSX.writeFile(wb, 'Exp_vs_Bgt_'+this.employeeId  +'.xlsx');
+
+}
+
 
    resetSearch(){
     this.searchText = '';
